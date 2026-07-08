a82243f0072607cb4a85feb86ef276c7cee78306
umceko <umutcevdetkocak@gmail.com>
2026-07-08 04:54:53 +0300

    convert to flake + home-manager input

--- configuration.nix
+++ configuration.nix
@@ -4,14 +4,10 @@
 
 { config, pkgs, ... }:
 
-let
-  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-26.05.tar.gz;
-in
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      (import "${home-manager}/nixos")
     ];
 
   # Bootloader.
---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "umceko's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+    # home-manager as a proper flake input, replacing the fetchTarball.
+    # `follows` makes it use the SAME nixpkgs as above (no second copy).
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # `nixos` here must match the `#nixos` you build with below.
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+      ];
+    };
+  };
+}
