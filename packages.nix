e5c9fd21799e76cda5e41991c39ca8b69de3515c
umceko <umutcevdetkocak@gmail.com>
2026-07-08 15:42:58 +0300

    add libnotify (notify-send)

--- packages.nix
+++ packages.nix
@@ -51,7 +51,7 @@
     kdePackages.kdenlive kdePackages.filelight kdePackages.okular kdePackages.kcalc
 
     # ── Wayland / Hyprland desktop stack ───────────────────────────
-    psmisc waybar wpaperd rofi wlogout swaynotificationcenter waypaper
+    psmisc libnotify waybar wpaperd rofi wlogout swaynotificationcenter waypaper
     nwg-dock-hyprland nwg-displays nwg-look
     hyprlock hypridle hyprpaper hyprpicker hyprshade hyprsunset
     hyprpolkitagent
